c059951f8e23ad3a839d80ce6a0f8ce38bbc9c96
Master <victor2001v10@gmail.com>
2024-08-17 19:17:49 +0100

    JEI integration

---
+++ src/main/java/net/vector/weaponseffect/compat/JEIWeaponsEffectPlugin.java
@@ -0,0 +1,34 @@
+package net.vector.weaponseffect.compat;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IGuiHandlerRegistration;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.resources.ResourceLocation;
+import net.vector.weaponseffect.WeaponsEffect;
+
+@JeiPlugin
+public class JEIWeaponsEffectPlugin implements IModPlugin {
+    @Override
+    public ResourceLocation getPluginUid() {
+        return ResourceLocation.fromNamespaceAndPath(WeaponsEffect.MOD_ID, "jei_plugin");
+
+}
+
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registration) {
+        IModPlugin.super.registerCategories(registration);
+    }
+
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+        IModPlugin.super.registerRecipes(registration);
+    }
+
+    @Override
+    public void registerGuiHandlers(IGuiHandlerRegistration registration) {
+        IModPlugin.super.registerGuiHandlers(registration);
+    }
+}
